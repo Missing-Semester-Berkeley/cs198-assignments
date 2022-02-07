ee3c898bc9f6f9c5af60822210a37549bdd03e91
Addison Chan <addison95132@gmail.com>
2022-02-07 09:39:37 -0800

    Various test improvements

--- homework_01/tests/tests.py
+++ homework_01/tests/tests.py
@@ -1,5 +1,7 @@
 import os
 import os.path
+import stat
+import subprocess
 import unittest
 
 MYLS_FILENAME = "myls"
@@ -14,18 +16,29 @@ def strip_spaces_and_newlines(s):
     return s.strip().replace("\n", "").replace("\r", "")
 
 
+def execute_file(filename, timeout=None):
+    st = os.stat(filename)
+    assert os.path.isfile(filename), f"File '{filename}' does not exist"
+    assert bool(st.st_mode & stat.S_IXUSR), f"File '{filename}' is not executable"
+    return subprocess.run(filename, capture_output=True, shell=True, timeout=timeout)
+
+
+def execute_command(command, timeout=None):
+    return subprocess.run(command, capture_output=True, shell=True, timeout=timeout)
+
+
 class HW01(unittest.TestCase):
     def test_1_myls(self):
         assert os.path.isfile(MYLS_FILENAME), f"File '{MYLS_FILENAME}' does not exist"
         with open(MYLS_FILENAME) as f:
             lines = [line for line in f.readlines() if len(line) > 1]  # Filter out 'intentional' line (non-whitespace)
-            assert len(lines) >= 1, f"File '{SEMESTER_FILENAME}' contains too few lines"
-            assert strip_spaces_and_newlines(lines[-1])[:len("ls")] == "ls", f"File '{MYLS_FILENAME}' does not contain an ls command"
+            assert len(lines) >= 1, f"File '{MYLS_FILENAME}' contains too few lines"
+            assert strip_spaces_and_newlines(lines[-1])[:len("ls")] == "ls", \
+                f"File '{MYLS_FILENAME}' does not contain an ls command"
 
     def test_2_marco(self):
-        import subprocess
         assert os.path.isfile(MARCO_FILENAME), f"File '{MARCO_FILENAME}' does not exist"
-        res = subprocess.run("./test_marco", capture_output=True, shell=True).stdout.decode().split()
+        res = execute_file("./test_marco").stdout.decode().split()
 
         assert len(res) == 2, "There were more than 2 lines of output in test_marco. This is a bug!"
         assert res[0] == res[1], f"""Marco and polo do not result in the same directories. Compare:
@@ -34,18 +47,15 @@ class HW01(unittest.TestCase):
 """
 
     def test_3_debug_failing_command(self):
-        import subprocess
-        assert os.path.isfile(DEBUG_FAILING_COMMAND), f"File '{DEBUG_FAILING_COMMAND}' does not exist"
-        res = subprocess.run(f"./{DEBUG_FAILING_COMMAND}", capture_output=True, shell=True, timeout=3).stdout.decode().split("\n")
+        res = execute_file(f"./{DEBUG_FAILING_COMMAND}", timeout=3).stdout.decode().split("\n")
 
         assert "Something went wrong" in res, "Stdout was not captured"
         assert "The error was using magic numbers" in res, "Stderr was not captured"
         assert "Everything went according to plan" not in res, "Do not capture successful run outputs"
 
     def test_4_tar_html(self):
-        import subprocess
         assert os.path.isfile(TAR_HTML), f"File '{TAR_HTML}' does not exist"
-        res = subprocess.run(f"./{TAR_HTML}", capture_output=True, shell=True, timeout=3).stdout.decode().split("\n")
+        res = execute_file(f"./{TAR_HTML}").stdout.decode().split("\n")
         assert os.path.isfile(TAR_HTML_TARBALL), f"Tarball '{TAR_HTML_TARBALL}' does not exist"
-        res = subprocess.run(f"tar --list -f {TAR_HTML_TARBALL}", capture_output=True, shell=True, timeout=3).stdout.decode().split("\n")
+        res = execute_command(f"tar --list -f {TAR_HTML_TARBALL}").stdout.decode().split("\n")
         assert len(res) == 3, f"Expected 2 files but found {len(res)-1} in tarball."
